202c7ee6b468e4127371f323a0282d4c1622c191
Nathan Lima <nathanls.dev@gmail.com>
2024-04-10 18:08:32 -0300

    fundamentals: class - data types - date

--- fundamentals/Program.cs
+++ fundamentals/Program.cs
@@ -5,3 +5,20 @@ Person citizen = new Person();
 citizen.Name = "Derp";
 citizen.Age = 59;
 citizen.Presentation();
+
+Console.WriteLine("\n ---------------------------------------- \n");
+
+string salutation = "Hello, you're welcome";
+int quantity = 1;
+double height = 1.80;
+decimal price = 1.80M;
+
+Console.WriteLine($"value of salutation: {salutation}");
+Console.WriteLine($"value of quantity: {quantity}");
+Console.WriteLine($"value of height: {height.ToString("0.00")}");
+Console.WriteLine($"value of price: {price}");
+
+Console.WriteLine("\n ---------------------------------------- \n");
+
+DateTime actualDate = DateTime.Now.AddDays(3);
+Console.WriteLine(actualDate);
