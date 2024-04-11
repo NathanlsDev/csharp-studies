c2f8773eec1c46043a26fd2a6c9e6ece0363302c
Nathan Lima <nathanls.dev@gmail.com>
2024-04-11 12:59:12 -0300

    introducing arrays and lists

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arrayInt = new int [3];
+
+arrayInt[0] = 10;
+arrayInt[1] = 20;
+arrayInt[2] = 30;
+
+// Array.Resize(ref arrayInt, arrayInt.Length * 2);
+int[] arrayIntDouble = new int[arrayInt.Length * 2];
+Array.Copy(arrayInt, arrayIntDouble, arrayInt.Length);
+
+Console.WriteLine("\nfor loop:");
+for (int i = 0; i < arrayInt.Length; i++){
+  Console.WriteLine($"index: {i}: value: {arrayInt[i]}");
+}
+
+Console.WriteLine("\nforeach:");
+foreach (int value in arrayInt){
+  Console.WriteLine($"value: {value}");
+}
---
+++ List/Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> listString = new List<string>();
+
+listString.Add("SP");
+listString.Add("BH");
+listString.Add("MG");
+listString.Add("RJ");
+
+Console.WriteLine($"Items in my list: {listString.Count} - List capacity: {listString.Capacity}");
+
+listString.Add("SC");
+Console.WriteLine($"Items in my list: {listString.Count} - List capacity: {listString.Capacity}");
+
+listString.Remove("MG");
+Console.WriteLine($"Items in my list: {listString.Count} - List capacity: {listString.Capacity}");
+
+
+// Console.WriteLine("\nfor loop:");
+// for (int i = 0; i < listString.Count; i++){
+//   Console.WriteLine($"index: {i} - value: {listString[i]}");
+// }
+
+// Console.WriteLine("\nforeach:");
+// foreach (string str in listString) { 
+//   Console.WriteLine(str);
+// }
