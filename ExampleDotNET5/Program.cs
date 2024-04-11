2ab7cc4ccc3ece42084fab5d4690f315ef480aff
Nathan Lima <nathanls.dev@gmail.com>
2024-04-11 09:47:22 -0300

    comparative between .net5 and .net6+

---
+++ ExampleDotNET5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; 
+
+namespace ExampleDotNET5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
