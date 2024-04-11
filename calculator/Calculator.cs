33b4abac7d6c99530d4e43d2baec0d8185c0596e
Nathan Lima <nathanls.dev@gmail.com>
2024-04-10 22:36:00 -0300

    basic calculator - Math lib

---
+++ calculator/Calculator.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace calculator{
+  public class Calculator  {
+    public void Sum(int x, int y){
+      Console.WriteLine($"{x} + {y} = {x + y}");
+    }
+
+    public void Subtract(int x, int y){
+      Console.WriteLine($"{x} - {y} = {x - y}");
+    }
+
+    public void Multiplication(int x, int y){
+      Console.WriteLine($"{x} x {y} = {x * y}");
+    }
+
+    public void Division(float x, float y){
+      Console.WriteLine($"{x} / {y} = {(x / y).ToString("0.00")}");
+    }
+
+    public void Power (int x, int y){
+      double power = Math.Pow(x, y); 
+      Console.WriteLine($"{x} ^ {y} = {power}");
+    }
+
+    public void Sine(double angle){
+      double radian = angle * (Math.PI / 180);
+      double sine = Math.Sin(radian);
+      Console.WriteLine($"The sine of {angle}° = {Math.Round(sine, 4)}");
+    }
+
+    public void Cosine(double angle){
+      double radian = angle * (Math.PI / 180);
+      double cosine = Math.Cos(radian);
+      Console.WriteLine($"The cosine of {angle}° = {Math.Round(cosine, 4)}");
+    }
+
+    public void Tangent(double angle){
+      double radian = angle * (Math.PI / 180);
+      double tangent = Math.Tan(radian);
+      Console.WriteLine($"The tangent of {angle}° = {Math.Round(tangent, 4)}");
+    }
+
+    public void Square(double x){
+      double square = Math.Sqrt(x);
+      Console.WriteLine($"Square root of {x} = {square}");      
+    }
+  }
+}
---
+++ calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using calculator;
+
+Calculator calc = new Calculator();
+
+calc.Sum(10, 30);
+calc.Subtract(10, 30);
+calc.Multiplication(10, 30);
+calc.Division(10, 30);
+calc.Power(3, 3);
+calc.Sine(30);
+calc.Cosine(30);
+calc.Tangent(30);
+calc.Square(9);
