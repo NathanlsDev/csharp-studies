97b95a5a5f24216f624845a5a19240ab66de2ebc
Nathan Lima <nathanls.dev@gmail.com>
2024-04-12 20:47:55 -0300

    handling currency models and date types

---
+++ ExploringCSharp/CurrenciesHandling/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+Console.WriteLine(monetaryValue.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+decimal monetaryValue = 1582.25M;
+
+Console.WriteLine(percentage.ToString("P"));
+double percentage = .3421;
---
+++ ExploringCSharp/DateHandling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+string stringDate = "2025-13-09 18:00";
+
+// DateTime date = DateTime.Now;
+// DateTime date = DateTime.Parse(stringDate);
+
+bool success = DateTime
+  .TryParseExact
+  (
+    stringDate, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture,
+    DateTimeStyles.None, out DateTime date
+  );
+
+  if (success)
+  {
+    Console.WriteLine($"Success converted date: {date}");
+  }
+  else 
+  {
+    Console.WriteLine($"${stringDate} isn't a valid date!");
+  }
+
+// Console.WriteLine(date.ToString("dd/MM/yyyy HH:mm"));
+// Console.WriteLine(date.ToShortDateString());
+// Console.WriteLine(date.ToShortTimeString());
+// Console.WriteLine(date);
