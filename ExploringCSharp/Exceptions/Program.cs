912932032374fecac635aa80e1e5cffaef3fa9b5
Nathan Lima <nathanls.dev@gmail.com>
2024-04-13 21:51:39 -0300

    exceptions

---
+++ ExploringCSharp/Exceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+  string[] lines = File.ReadAllLines("./KnightsOfCydoni458a.txt");
+
+  foreach (string line in lines)
+  {
+    Console.WriteLine(line);
+  }
+} catch(Exception except)
+{
+  Console.WriteLine($"A generic exception occurred: {except.Message}");
+}
