1c18064ec1498c187cab4053a750a475ea3d435e
Nathan Lima <nathanls.dev@gmail.com>
2024-04-15 18:33:08 -0300

    introduction to Queue

---
+++ ExploringCSharp/Queue/Program.cs
@@ -0,0 +1,19 @@
+﻿Queue<int> queue = new Queue<int>();
+
+queue.Enqueue(2);
+queue.Enqueue(4);
+queue.Enqueue(6);
+queue.Enqueue(8);
+
+foreach(int element in queue)
+{
+  Console.WriteLine(element);
+}
+
+Console.WriteLine($"\nRemoved element {queue.Dequeue()}\n");
+queue.Enqueue(10);
+
+foreach(int element in queue)
+{
+  Console.WriteLine(element);
+}
