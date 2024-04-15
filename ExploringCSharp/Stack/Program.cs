9f70ee718499be74818be3b68f7ce76434ee870a
Nathan Lima <nathanls.dev@gmail.com>
2024-04-15 18:44:25 -0300

    introduction to Stack

---
+++ ExploringCSharp/Stack/Program.cs
@@ -0,0 +1,19 @@
+﻿Stack<int> stack = new Stack<int>();
+
+stack.Push(3);
+stack.Push(5);
+stack.Push(7);
+stack.Push(9);
+
+foreach(int number in stack)
+{
+  Console.WriteLine(number);
+}
+
+Console.WriteLine($"\nRemoved element {stack.Pop()}\n");
+stack.Push(11);
+
+foreach(int number in stack)
+{
+  Console.WriteLine(number);
+}
