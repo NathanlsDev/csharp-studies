b5b403912dd11bddfd5bf71817e6afd970a9efc6
Nathan Lima <nathanls.dev@gmail.com>
2024-04-15 17:58:42 -0300

    specific exceptions try-catch-finally

--- ExploringCSharp/Exceptions/Program.cs
+++ ExploringCSharp/Exceptions/Program.cs
@@ -1,12 +1,25 @@
 ﻿try
-{
-  string[] lines = File.ReadAllLines("./KnightsOfCydoni458a.txt");
-
-  foreach (string line in lines)
   {
-    Console.WriteLine(line);
+    string[] lines = File.ReadAllLines("./KnightsOfCydoni458a.txt"); //wrong path
+
+    foreach (string line in lines)
+    {
+      Console.WriteLine(line);
+    }
   }
-} catch(Exception except)
-{
-  Console.WriteLine($"A generic exception occurred: {except.Message}");
-}
+catch(FileNotFoundException except)
+  {
+    Console.WriteLine($"An error occurred in the reading of the file. File not found: {except.Message}");
+  }
+catch(DirectoryNotFoundException except)
+  {
+    Console.WriteLine($"An error occurred in the reading of the file. Folder path not found: {except.Message}");
+  }
+catch(Exception except)
+  {
+    Console.WriteLine($"A generic exception occurred: {except.Message}");
+  }
+finally
+  {
+    Console.WriteLine("Default output.");
+  }
