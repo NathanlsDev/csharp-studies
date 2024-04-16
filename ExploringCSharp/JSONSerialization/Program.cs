ac48cd60e376d30753165e4149b316972c512d41
Nathan Lima <nathanls.dev@gmail.com>
2024-04-16 20:54:45 -0300

    JSON serialization

---
+++ ExploringCSharp/JSONSerialization/Program.cs
@@ -0,0 +1,7 @@
+﻿using JSONSerialization.Models;
+using Newtonsoft.Json;
+
+Sale transaction = new Sale(1, "Office supplies", 25.00M);
+
+string serialized = JsonConvert.SerializeObject(transaction, Formatting.Indented);
+Console.WriteLine(serialized);
