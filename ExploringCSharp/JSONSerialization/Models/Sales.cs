2b9d102a8c01197ef9020b909a6799f86be4d8a7
Nathan Lima <nathanls.dev@gmail.com>
2024-04-17 11:49:04 -0300

    collection serialization and overwiew ISO8601 of date format

--- ExploringCSharp/JSONSerialization/Models/Sales.cs
+++ ExploringCSharp/JSONSerialization/Models/Sales.cs
@@ -7,15 +7,17 @@ namespace JSONSerialization.Models
 {
     public class Sale
     {
-        public Sale(int id, string product, decimal price)
+        public Sale(int id, string product, decimal price, DateTime saledate)
         {
             Id = id;
             Product = product;
             Price = price;
+            SaleDate = saledate;
         }
 
         public int Id { get; set; }
         public string Product { get; set; }
         public decimal Price { get; set; }
+        public DateTime SaleDate { get; set; }
     }
 }
--- ExploringCSharp/JSONSerialization/Program.cs
+++ ExploringCSharp/JSONSerialization/Program.cs
@@ -1,7 +1,18 @@
 ﻿using JSONSerialization.Models;
 using Newtonsoft.Json;
 
-Sale transaction = new Sale(1, "Office supplies", 25.00M);
+DateTime actualDate = DateTime.Now;
 
-string serialized = JsonConvert.SerializeObject(transaction, Formatting.Indented);
+List<Sale> salesList = new List<Sale>();
+
+Sale transaction = new Sale(1, "Office supplies", 25.00M, actualDate);
+Sale transaction2 = new Sale(2, "Software License", 110.00M, actualDate);
+
+salesList.Add(transaction);
+salesList.Add(transaction2);
+
+string serialized = JsonConvert.SerializeObject(salesList, Formatting.Indented);
+
+File.WriteAllText("archives/sales.json", serialized);
+
 Console.WriteLine(serialized);
