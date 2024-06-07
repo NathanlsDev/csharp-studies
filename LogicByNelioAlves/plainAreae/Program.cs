ac2b2d65236e02118d4df2f0d4157a191de9e2b5
Nathan Lima <nathanls.dev@gmail.com>
2024-06-07 15:49:31 -0300

    exercis calc area and price

---
+++ LogicByNelioAlves/plainAreae/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Fazer um programa para ler as medidas da largura e comprimento de um tereno retangular com uma casa decimal, bem como
+ * o valor do metro quadrado do terreno com duas casas decimais. Em seguida, o programa deve mostrar o valor da área do terreno,
+ * bem como o valor do preço do terreno, ambos com duas casas decimais.
+ */
+
+using System;
+
+namespace plainArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the land width:");
+            double landWidth = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type the land length:");
+            double landLength = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Price per square meter:");
+            double meterValue = double.Parse(Console.ReadLine());
+
+            double totalLandArea = landWidth * landLength;
+            double totalLandPrice = totalLandArea * meterValue;
+
+            Console.WriteLine($"AREA = {totalLandArea.ToString("F2")}");
+            Console.WriteLine($"PRICE = {totalLandPrice.ToString("F2")}");            
+        }
+    }
+}
