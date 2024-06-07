17443c48f8d9f49e7ec8a9795274886c9239f057
Nathan Lima <nathanls.dev@gmail.com>
2024-06-07 17:53:12 -0300

    ex triangle  area, perimeter, diagonal

---
+++ LogicByNelioAlves/area1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace area1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double triangleBase, triangleHeight, area, perimeter, diagonal;
+
+            Console.WriteLine("Define triangle base:");
+            triangleBase = double.Parse(Console.ReadLine());
+            Console.WriteLine("Define triangle heigth:");
+            triangleHeight = double.Parse(Console.ReadLine());
+
+            area = triangleBase * triangleHeight;
+            perimeter = 2 * (triangleBase + triangleHeight);
+            diagonal = Math.Sqrt(Math.Pow(triangleBase, 2.0) + Math.Pow(triangleHeight, 2.0));
+
+            Console.WriteLine($"Area = {area.ToString("F4")}");
+            Console.WriteLine($"Area = {perimeter.ToString("F4")}");
+            Console.WriteLine($"Area = {diagonal.ToString("F4")}");
+
+
+
+        }
+    }
+}
