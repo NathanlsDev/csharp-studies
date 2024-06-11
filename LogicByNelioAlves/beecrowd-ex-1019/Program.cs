10ea5130881c3281e84cdddb0004896ff16b024d
Nathan Lima <nathanls.dev@gmail.com>
2024-06-11 12:01:40 -0300

    beecrowd ex 1019

---
+++ LogicByNelioAlves/beecrowd-ex-1019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace beecrowd1019
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int N, hours,minutes, seconds, rest;
+
+            Console.WriteLine("Input seconds to be converted in time:");
+            N = int.Parse(Console.ReadLine());
+
+            hours = N / 3600;
+            rest = N % 3600;
+
+            minutes = rest / 60;
+            seconds = rest % 60;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
