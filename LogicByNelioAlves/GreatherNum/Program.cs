9e3335fe6f5326e63baad9adf030418345c93eaf
Nathan Lima <nathanls.dev@gmail.com>
2024-06-17 18:06:22 -0300

    firts look in functions sintax

---
+++ LogicByNelioAlves/GreatherNum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GreeatherNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type tree numbers:");
+            int n1, n2, n3;
+
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+
+            double result = Greather(n1, n2, n3);
+
+            Console.WriteLine($"The Greather Number is: {result}");
+        }
+
+        static int Greather(int a, int b, int c) 
+        {
+            if (a > b && a > c)
+            {
+                return a;
+            }
+
+            if (b > c)
+            {
+                return b;
+            }
+
+            return c;            
+        }
+    }
+}
