2ec199c5c42048ecd56d23c564536af220de6da7
Nathan Lima <nathanls.dev@gmail.com>
2024-06-19 19:22:19 -0300

    managing products - override - system.reflection

---
+++ OOP/ProductManager/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductManager
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Qtd;
+
+        public double TotalValueStock(double price, int qtd) 
+        {
+            return price * qtd;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} | Price: ${Price.ToString("F2")} | Quantity: {Qtd} | Total value stock: {TotalValueStock(Price, Qtd).ToString("F2")}.";
+        }
+
+
+        public void AddProduct(int units) 
+        {
+            Qtd += units;
+        }
+
+        public void RemoveProduct(int units)
+        {
+            Qtd -= units;
+        }
+    }
+}
---
+++ OOP/ProductManager/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace ProductManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            Console.WriteLine("\nEnter product data\n");
+            Console.Write("Name: ");
+            product.Name = Console.ReadLine();
+
+            Console.Write("Price: ");
+            product.Price = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantity in stock: ");
+            product.Qtd = int.Parse(Console.ReadLine());
+
+            DisplayProductData(product);
+        }
+
+        static void DisplayProductData(Product product)
+        {
+            Console.WriteLine($"\nProduct data: {product}");
+            ManageProductStock(product);
+        }
+
+        static void ManageProductStock(Product product)
+        {
+            int units;
+
+            Console.Write("\nDo you want to 'Add' or 'Remove' products? ");
+            string action = Console.ReadLine();
+
+            Console.Write($"\n{action} products quantity: ");
+            units = int.Parse(Console.ReadLine());
+
+            string methodName = $"{action}Product";
+            MethodInfo method = product.GetType().GetMethod(methodName);
+            method.Invoke(product, new object[] { units });
+
+            Console.WriteLine($"\nProduct data: {product}");
+        }
+    }
+}
